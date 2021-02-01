e83c79cfbaa9419f72e4af231e2691bc67f09202
imansawi <imansawi@hotmail.com>
2021-02-01 23:11:43 +0000

    A function tests if two arguments are equal

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION TESTS IF TWO ARGUMENTS ARE EQUAL
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return;
+};
+
+assertEqual("Lighthouse Labs","Bootcamp");
+assertEqual("Lighthouse Labs","Lighthouse Labs");
