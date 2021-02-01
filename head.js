7c4dfd2be2da28555cec856e8d091062db8c10a3
imansawi <imansawi@hotmail.com>
2021-02-01 23:19:52 +0000

    A function returns the head of an array

---
+++ head.js
@@ -0,0 +1,9 @@
+// FUNCTION RETURNS HEAD OF AN ARRAY
+const head = function(myArray) {
+  if (myArray.length > 0) {
+    let firstElement = myArray.shift();
+    console.log(firstElement);
+  }
+};
+
+head(["Welcome", "Lighthouse Labs","Bootcamp"]);
