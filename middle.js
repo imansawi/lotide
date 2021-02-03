602605e1af734abb00c0400ed3257e955040505e
imansawi <imansawi@hotmail.com>
2021-02-03 23:08:55 +0000

    created function outputs the middle element/s in an array

---
+++ middle.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const middle = function(myArray) {
+  let middleElement = [];
+  if  (myArray.length < 3) {
+    return middleElement;
+  } else if (myArray.length % 2 === 0) {
+    middleElement.push(myArray[myArray.length / 2 - 1]);
+    middleElement.push(myArray[myArray.length / 2]);
+  } else {
+    middleElement.push(myArray[(myArray.length / 2).toFixed() - 1]);
+  }
+  return middleElement;
+};
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 9, 4, 7]));
+console.log(middle([7, 6, 3, 4, 1, 2]));
+module.exports = middle;
