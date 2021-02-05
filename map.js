99063b86901dad27d2ea5e8826865b733f79f268
imansawi <imansawi@hotmail.com>
2021-02-05 01:21:14 +0000

    created map function to map array of string with an array of the first letter of each of these strings

---
+++ map.js
@@ -0,0 +1,13 @@
+// Function maps an each element of an array of strings with an array of the first letterof these strings
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
