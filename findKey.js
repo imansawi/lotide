b518e93eb3dc8c56e42922a4cc2727606f2c59d3
imansawi <imansawi@hotmail.com>
2021-02-05 00:07:05 +0000

    Created function returns the key of an object that equals to a certain condtion.

---
+++ findKey.js
@@ -0,0 +1,38 @@
+const assertEqual = require("./assertEqual.js");
+// Function returns the key of an object that equals to a certain condtion.
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+  return undefined;
+};
+
+// Test code
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
+
+// Assertion
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma");
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3),"noma");
