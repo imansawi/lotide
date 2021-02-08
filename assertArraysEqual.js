8e4e402013e498fc473e133a1ad8c6d258a9bb3c
imansawi <imansawi@hotmail.com>
2021-02-08 23:07:25 +0000

    Refactored assertion

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,4 +6,7 @@ const assertArraysEqual = function(array1, array2, result) {
   assertEqual(eqArrays(array1, array2), result);
 };
 
+//console.log(assertArraysEqual([1, 2, 5], [5, 1, 2], true));
+//console.log(assertArraysEqual([1, 2, 5], [5, 1, 3], true));
+
 module.exports = assertArraysEqual;
