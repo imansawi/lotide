60a00a4c9d07ac1c0c9ec22b7c8789d33b1e4b54
imansawi <imansawi@hotmail.com>
2021-02-08 22:48:14 +0000

    Refactored assertion

--- tail.js
+++ tail.js
@@ -1,12 +1,12 @@
 // FUNCTION RETURNS TAIL OF AN ARRAY
 const tail = function(myArray) {
-  if (myArray.length > 0) {
-    let x = myArray.shift();
-    x = "";
+  if (myArray.length <= 1) {
     return myArray;
+  } else {
+    return myArray.slice(1);
   }
 };
 
-console.log(tail(["Welcome!", "Lighthouse", "Labs","Bootcamp"]));
+//console.log(tail(["Welcome!", "Lighthouse", "Labs","Bootcamp"]));
 
 module.exports = tail;
